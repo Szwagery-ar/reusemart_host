dfb43b56866dad1c9e0d36322297885e40c37cc8
Stinnery69 <220712138@students.uajy.ac.id>
2025-06-07 15:28:50 +0700

    calon istri gweh itu barista

--- src/components/AdminSidebar/AdminSidebar.jsx
+++ src/components/AdminSidebar/AdminSidebar.jsx
@@ -6,7 +6,7 @@ import LogoutButton from '../LogoutButton/LogoutButton';
 
 
 
-import { LayoutDashboard, Package, Gift, LogOut, ContactRound, ShoppingBag, HeartHandshake, HandCoins, Banknote, Truck, FolderPlus } from 'lucide-react';
+import { LayoutDashboard, Package, Gift, LogOut, ContactRound, ShoppingBag, HeartHandshake, HandCoins, Banknote, Truck, FolderPlus, FileChartColumnIncreasing } from 'lucide-react';
 import { UserRound, Handshake, Ribbon } from 'lucide-react';
 
 import './AdminSidebar.css';
@@ -25,6 +25,7 @@ const menuByRole = {
     ],
     OWNER: [
         { name: 'Dashboard', path: '/admin', icon: LayoutDashboard },
+        { name: 'Laporan', path: '/admin/laporan', icon: FileChartColumnIncreasing },
         { name: 'Request Donasi', path: '/admin/reqdonasi', icon: ShoppingBag },
         { name: 'Donasi', path: '/admin/donasi', icon: HeartHandshake },
 
@@ -32,7 +33,7 @@ const menuByRole = {
     ADMIN: [
         { name: 'Dashboard', path: '/admin', icon: LayoutDashboard },
         { name: 'Merchandise', path: '/admin/merchandise', icon: Gift },
-        { name: 'Data Pegawai', path: '/admin/pegawai', icon: ContactRound},
+        { name: 'Data Pegawai', path: '/admin/pegawai', icon: ContactRound },
         { name: 'Data Pembeli', path: '/admin/pembeli', icon: UserRound },
         { name: 'Data Organisasi', path: '/admin/organisasi', icon: Ribbon },
     ],
@@ -45,8 +46,8 @@ const menuByRole = {
     ],
     CS: [
         { name: 'Dashboard', path: '/admin', icon: LayoutDashboard },
-        { name: 'Data Penitip', path: '/admin/penitip', icon: Handshake  },
-        { name: 'Data Pembayaran', path: '/admin/konfirmasi-pembayaran', icon: Banknote  },
+        { name: 'Data Penitip', path: '/admin/penitip', icon: Handshake },
+        { name: 'Data Pembayaran', path: '/admin/konfirmasi-pembayaran', icon: Banknote },
         { name: 'Diskusi', path: '/admin/diskusi', icon: Handshake },
     ],
     QC: [
@@ -96,11 +97,10 @@ export default function AdminSidebar() {
                     return (
                         <a key={item.name} href={item.path} className='flex justify-end'>
                             <div
-                                className={`flex items-center gap-2 px-4 py-3 w-100 ml-6 text-sm ${
-                                    pathname === item.path
+                                className={`flex items-center gap-2 px-4 py-3 w-100 ml-6 text-sm ${pathname === item.path
                                         ? 'sidebar-highlight font-semibold bg-white text-indigo-800'
                                         : 'text-white hover:font-semibold hover:border-b-2 hover:border-white'
-                                }`}
+                                    }`}
                             >
                                 <Icon className="w-5 h-5" />
                                 <h1 className='z-10'>{item.name}</h1>
@@ -109,9 +109,9 @@ export default function AdminSidebar() {
                     );
                 })}
             </nav>
-            <LogoutButton 
+            <LogoutButton
                 icon={LogOut}
-                className="flex items-center mt-2 gap-2 px-4 py-3 w-58 ml-6 text-sm text-white cursor-pointer hover:font-semibold hover:border-b-2 hover:border-white" 
+                className="flex items-center mt-2 gap-2 px-4 py-3 w-58 ml-6 text-sm text-white cursor-pointer hover:font-semibold hover:border-b-2 hover:border-white"
             />
         </aside>
     );
