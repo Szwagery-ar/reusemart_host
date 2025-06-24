2bcb315fbefbdfbf0764780bb9dad9f82be4bea4
Stinnery69 <220712138@students.uajy.ac.id>
2025-06-25 03:54:49 +0700

    upload bukti pakai blob

--- src/app/api/pembayaran/upload/route.js
+++ src/app/api/pembayaran/upload/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 import pool from "@/lib/db";
+import { put } from "@vercel/blob";
 
 export async function PUT(request) {
     try {
@@ -13,19 +14,43 @@ export async function PUT(request) {
             return NextResponse.json({ error: "File atau ID Transaksi tidak valid" }, { status: 400 });
         }
 
-        const bytes = await file.arrayBuffer();
-        const buffer = Buffer.from(bytes);
+        // const bytes = await file.arrayBuffer();
+        // const buffer = Buffer.from(bytes);
 
+        // const fileName = `bukti-${Date.now()}-${file.name}`;
+        // const filePath = path.join(process.cwd(), "public", "uploads", fileName);
+        // await fs.promises.writeFile(filePath, buffer);
+
+        // const imgUrl = `/uploads/${fileName}`;
+
+        // // Update ke tabel pembayaran
+        // await pool.query(
+        //     `UPDATE pembayaran SET img_bukti_transfer = ? WHERE id_transaksi = ?`,
+        //     [imgUrl, id_transaksi]
+        // );
+
+
+        //pakai blob 
+        const buffer = Buffer.from(await file.arrayBuffer());
         const fileName = `bukti-${Date.now()}-${file.name}`;
-        const filePath = path.join(process.cwd(), "public", "uploads", fileName);
-        await fs.promises.writeFile(filePath, buffer);
 
-        const imgUrl = `/uploads/${fileName}`;
+        // Upload ke Vercel Blob Storage
+        const { url } = await put(fileName, buffer, {
+            access: "public", // bisa diakses langsung
+        });
 
-        // Update ke tabel pembayaran
+        // Simpan URL ke database
         await pool.query(
             `UPDATE pembayaran SET img_bukti_transfer = ? WHERE id_transaksi = ?`,
-            [imgUrl, id_transaksi]
+            [url, id_transaksi]
+        );
+
+        return NextResponse.json(
+            {
+                message: "Bukti pembayaran berhasil diupload dan disimpan.",
+                img_bukti_transfer: url,
+            },
+            { status: 200 }
         );
 
         return NextResponse.json({ message: "Bukti pembayaran berhasil diupload dan disimpan.", img_bukti_transfer: imgUrl }, { status: 200 });
